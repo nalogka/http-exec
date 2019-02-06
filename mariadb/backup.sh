b92338059a36b013ae65e40bf5dd5840b24e0812
Ivan Ochagov <ivan@ochagov.net>
2019-02-06 15:25:00 +0600

    Добавлен список небекапируемых баз

--- mariadb/backup.sh
+++ mariadb/backup.sh
@@ -2,6 +2,7 @@
 if [ -d "/tmp/backup" ]; then
 	rm -fr /tmp/backup
 fi
-mariabackup -u root -p$MYSQL_ROOT_PASSWORD --backup --target-dir=/tmp/backup
+EXCLUDE_DB="nalogka-sandbox_core-deals nalogka-sandbox_delivery-cdek nalogka-sandbox_depositary-b2p nalogka-sandbox_users nalogka_admin_sandbox nalogka_delivery_cdek_sandbox"
+mariabackup -u root -p$MYSQL_ROOT_PASSWORD --backup --databases-exclude="$EXCLUDE_DB" --target-dir=/tmp/backup
 tar cz -C /tmp/backup .
 rm -fr /tmp/backup
