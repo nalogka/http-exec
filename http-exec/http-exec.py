bb458e93180fdb17d240c780e1899eca65ac041e
Ivan Ochagov <ivan@ochagov.net>
2019-02-08 17:42:17 +0600

    Версия 2

--- http-exec/http-exec.py
+++ http-exec/http-exec.py
@@ -7,31 +7,45 @@ import os
 import subprocess
 class Server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        try:
-            with open('/run/secrets/bacula-auth') as f:
-                secret = f.readline()
-        except:
-            secret = "BMWD3FEwlBageKHMCOJpqZBCNCBFbYbH\n"
-        authenticated = False
-        for h in self.headers._headers:
-            if h[0] == "X-Nalogka-Auth" and h[1]+"\n" == secret:
-                authenticated = True
-        if not authenticated:
-            self.send_error(403, 'Unauthorized')
+        if os.environ.get('HTTP_EXEC_USE_AUTHENTICATION') is not None and (os.environ['HTTP_EXEC_USE_AUTHENTICATION'].lower == "false" or os.environ['HTTP_EXEC_USE_AUTHENTICATION'].lower == "no"):
+            use_authentication = False
         else:
-            pipe = subprocess.Popen(["/bin/sh", "/http-exec/backup.sh"],stdout=subprocess.PIPE)
-            self.send_response(200, 'OK')
-            self.send_header('Content-type', 'application/gzip')
-            self.send_header('Content-Disposition', 'attachment; filename="backup.tar.gz"')
-            self.end_headers()
-            piece_size = 4096 # 4 KiB
+            use_authentication = True
+        if use_authentication:
+            try:
+                with open(os.environ['HTTP_EXEC_SECRET_PATH']) as f:
+                    secret = f.readline()
+            except:
+                secret = ""
+            authenticated = False
+            if secret != "":
+                for h in self.headers._headers:
+                    if h[0] == "X-Nalogka-Auth" and h[1]+"\n" == secret:
+                        authenticated = True
+            if not authenticated:
+                self.send_error(403, 'Unauthorized')
+        if not use_authentication or (use_authentication and authenticated):
+            execpath = os.environ['HTTP_EXEC_BINARY_FOLDER_PATH']
+            if execpath.endswith('/'):
+                execpath = execpath[:-1]
+            if self.path.count('/') > 1:
+                self.send_error(404, 'Not found')
+            elif os.path.isfile(execpath+self.path) and os.access(execpath+self.path, os.X_OK):
+                pipe = subprocess.Popen([execpath+self.path],stdout=subprocess.PIPE,shell=True)
+                self.send_response(200, 'OK')
+                self.send_header('Content-type', 'application/gzip')
+                self.send_header('Content-Disposition', 'attachment; filename="backup.tar.gz"')
+                self.end_headers()
+                piece_size = 4096 # 4 KiB
 
-            with pipe.stdout as in_file:
-                while True:
-                    piece = in_file.read(piece_size)
-                    if piece == b'':
-                        break # end of file
-                    self.wfile.write(piece)
+                with pipe.stdout as in_file:
+                    while True:
+                        piece = in_file.read(piece_size)
+                        if piece == b'':
+                            break # end of file
+                        self.wfile.write(piece)
+            else:
+                self.send_error(404, 'Not found')
 
 
     def serve_forever(port):
