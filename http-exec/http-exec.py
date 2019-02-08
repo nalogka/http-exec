8d561db8c54d947bc5f9ef5c431ed3e7cfc184f3
Ivan Ochagov <ivan@ochagov.net>
2019-02-08 18:19:32 +0600

    Упрощение кода

--- http-exec/http-exec.py
+++ http-exec/http-exec.py
@@ -6,12 +6,9 @@ import random
 import os
 import subprocess
 class Server(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if os.environ.get('HTTP_EXEC_USE_AUTHENTICATION') is not None and (os.environ['HTTP_EXEC_USE_AUTHENTICATION'].lower == "false" or os.environ['HTTP_EXEC_USE_AUTHENTICATION'].lower == "no"):
-            use_authentication = False
-        else:
-            use_authentication = True
-        if use_authentication:
+    def do_GET(self): 
+        # Проверка аутентификации
+        if os.environ.get('HTTP_EXEC_USE_AUTHENTICATION') is None or os.environ['HTTP_EXEC_USE_AUTHENTICATION'].lower == "true" or os.environ['HTTP_EXEC_USE_AUTHENTICATION'].lower == "yes":
             try:
                 with open(os.environ['HTTP_EXEC_SECRET_PATH']) as f:
                     secret = f.readline()
@@ -27,27 +24,26 @@ class Server(http.server.SimpleHTTPRequestHandler):
                         authenticated = True
             if not authenticated:
                 self.send_error(403, 'Unauthorized')
-        if not use_authentication or authenticated:
-            execpath = os.environ['HTTP_EXEC_BINARY_FOLDER_PATH']
-            if execpath.endswith('/'):
-                execpath = execpath[:-1]
-            if self.path.count('/') > 1:
-                self.send_error(404, 'Not found')
-            elif os.path.isfile(execpath+self.path) and os.access(execpath+self.path, os.X_OK):
-                pipe = subprocess.Popen([execpath+self.path],stdout=subprocess.PIPE,shell=True)
-                self.send_response(200, 'OK')
-                self.send_header('Content-type', 'application/octet-stream')
-                self.end_headers()
-                piece_size = 4096 # 4 KiB
-
-                with pipe.stdout as in_file:
-                    while True:
-                        piece = in_file.read(piece_size)
-                        if piece == b'':
-                            break # end of file
-                        self.wfile.write(piece)
-            else:
-                self.send_error(404, 'Not found')
+                return 0
+        execpath = os.environ['HTTP_EXEC_BINARY_FOLDER_PATH']
+        if execpath.endswith('/'):
+            execpath = execpath[:-1]
+        if self.path.count('/') > 1:
+            self.send_error(404, 'Not found')
+        elif os.path.isfile(execpath+self.path) and os.access(execpath+self.path, os.X_OK):
+            pipe = subprocess.Popen([execpath+self.path],stdout=subprocess.PIPE,shell=True)
+            self.send_response(200, 'OK')
+            self.send_header('Content-type', 'application/octet-stream')
+            self.end_headers()
+            piece_size = 4096 # 4 KiB
+            with pipe.stdout as in_file:
+                while True:
+                    piece = in_file.read(piece_size)
+                    if piece == b'':
+                        break # end of file
+                    self.wfile.write(piece)
+        else:
+            self.send_error(404, 'Not found')
 
 
     def serve_forever(port):
