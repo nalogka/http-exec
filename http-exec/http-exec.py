f2284e9b94ce0f65659fa565a59df3826215324b
Ivan Ochagov <ivan@ochagov.net>
2019-02-08 17:58:28 +0600

    Добавил проверку на символ конца строки при чтении секрета из файла

--- http-exec/http-exec.py
+++ http-exec/http-exec.py
@@ -15,12 +15,15 @@ class Server(http.server.SimpleHTTPRequestHandler):
             try:
                 with open(os.environ['HTTP_EXEC_SECRET_PATH']) as f:
                     secret = f.readline()
+                    if secret.endswith('\n'):
+                        secret = secret[:-1]
+                    f.close()
             except:
                 secret = ""
             authenticated = False
             if secret != "":
                 for h in self.headers._headers:
-                    if h[0] == "X-Nalogka-Auth" and h[1]+"\n" == secret:
+                    if h[0] == "X-Nalogka-Auth" and h[1] == secret:
                         authenticated = True
             if not authenticated:
                 self.send_error(403, 'Unauthorized')
@@ -33,8 +36,7 @@ class Server(http.server.SimpleHTTPRequestHandler):
             elif os.path.isfile(execpath+self.path) and os.access(execpath+self.path, os.X_OK):
                 pipe = subprocess.Popen([execpath+self.path],stdout=subprocess.PIPE,shell=True)
                 self.send_response(200, 'OK')
-                self.send_header('Content-type', 'application/gzip')
-                self.send_header('Content-Disposition', 'attachment; filename="backup.tar.gz"')
+                self.send_header('Content-type', 'application/octet-stream')
                 self.end_headers()
                 piece_size = 4096 # 4 KiB
 
